f9902702069080cbaf270eaf89d0965f9ecadcaa
Sonia Sherpa <sonia.sherpa828@gmail.com>
2022-05-17 20:12:30 -0600

    updated

--- Diamond/src/main_diamond.cpp
+++ Diamond/src/main_diamond.cpp
@@ -3,64 +3,57 @@
 
 using namespace std;
 
-struct Device {  // ~ interface in Java/C#
-    // its a good idea to only have a default constructor
-   private: string m_ip;
-   public: Device() { 
-       cout << "Device()" << endl;  
+struct Rectangle {  
+   private: string m_i;
+   public: Rectangle() { 
+       cout << "Rectangle()" << endl;  
        }  // default constructor only
-   virtual string ip() const { return m_ip; }
-   virtual void ip(const string &value) { m_ip = value; }
-   virtual ~Device() { cout << "~Device()" << endl; }
+   virtual string ip() const { return m_i; }
+   virtual void ip(const string &value) { m_i = value; }
+   virtual ~Rectangle() { cout << "~Rectangle()" << endl; }
 };
 
-struct Equipment;
-struct Cord;
+struct Side;
+struct Breadth;
 
-struct Cord : virtual Device {
+struct Breadth : virtual Rectangle {
     const int capacity;
-    Cord(int _capacity) : capacity(_capacity) { cout << "Cord" << endl; } 
-    virtual bool compatible(const Equipment &equipment) const;
+    Breadth(int _capacity) : capacity(_capacity) { cout << "Breadth" << endl; } 
+    virtual bool compatible(const Side &side) const;
     virtual int watts() const { return 120*capacity; }
-    virtual ~Cord() { cout << "~Cord()" << endl; }
+    virtual ~Breadth() { cout << "~Breadth()" << endl; }
 };
 
-struct Equipment : virtual Device {
+struct Side : virtual Rectangle {
     const int consumption;
-    Equipment(int _consumption) : consumption(_consumption) { }
+    Side(int _consumption) : consumption(_consumption) { }
 };
 
-bool Cord::compatible(const Equipment &equipment) const {
-  cout << "Cord::compatible()" << endl;
-  return (capacity >= equipment.consumption);
+bool Breadth::compatible(const Side &side) const {
+  cout << "Breadth::compatible()" << endl;
+  return (capacity >= side.consumption);
 }
 
-struct Fused : virtual Device {
+struct Length : virtual Rectangle {
     bool fuseOk;
-    Fused() : fuseOk(true) { cout << "Fused" << endl; }
+    Length() : fuseOk(true) { cout << "Length" << endl; }
 
     virtual void reset() { fuseOk = true; }
     virtual void trip() { fuseOk = false; }
-    virtual ~Fused() { cout << "~Fused()" << endl; }
+    virtual ~Length() { cout << "~Length()" << endl; }
 };
 
-struct FusedCord : Cord, Fused, virtual Device  {
-    FusedCord(int capacity) : Cord(capacity) { cout << "FusedCord" << endl; }
-   virtual bool compatible(const Equipment &equipment) const override {
-        cout << "FusedCord::compatible()" << endl;
+struct Perimeter : Breadth, Length, virtual Rectangle  {
+    Perimeter(int capacity) : Breadth(capacity) { cout << "Perimeter" << endl; }
+   virtual bool compatible(const Side &side) const override {
+        cout << "Perimeter::compatible()" << endl;
         if (!fuseOk) return false;
-        return Cord::compatible(equipment);
+        return Breadth::compatible(side);
     }
-    ~FusedCord() { cout << "~FusedCord" << endl; }
+    ~Perimeter() { cout << "~Perimeter" << endl; }
 };
 
-//     Device  (virtual base classes).
-//    |      \
-//   Cord      Fused (fuseOk, trip, reset)
-//      \       /
-//       \     /
-//       FusedCord (ip)
 
 int main(int argc, const char *argv[]) {
-    FusedCord fusedCord(15);
+    Perimeter perimeter(15);
 }
